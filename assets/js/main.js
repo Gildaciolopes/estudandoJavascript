3fa26155b6e5ebe33e461cbe7da35874f19061e7
Gildácio Lopes <gildacioj399@gmail.com>
2025-03-15 19:03:28 -0300

    feat: resolve the exercise

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,9 @@
 const paragrafos = document.querySelector(".paragrafos");
 const ps = paragrafos.querySelectorAll("p");
+
+const estilosBody = getComputedStyle(document.body);
+
+for (let p of ps) {
+  p.style.backgroundColor = estilosBody.backgroundColor;
+  p.style.color = "white";
+}
