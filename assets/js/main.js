ec22e9dfb1b028a222f7ec320beac4df795ac679
Gildácio Lopes <gildacioj399@gmail.com>
2025-03-16 20:25:27 -0300

    refactor: create a timer with setInterval

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,39 @@
-const paragrafos = document.querySelector(".paragrafos");
-const ps = paragrafos.querySelectorAll("p");
+function relogio() {
+  function getTimeFromSeconds(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString("pt-BR", {
+      hour12: false,
+      timeZone: "UTC",
+    });
+  }
 
-const estilosBody = getComputedStyle(document.body);
+  const relogio = document.querySelector(".relogio");
+  let segundos = 0;
+  let timer;
 
-for (let p of ps) {
-  p.style.backgroundColor = estilosBody.backgroundColor;
-  p.style.color = "white";
+  function iniciaRelogio() {
+    timer = setInterval(function () {
+      segundos++;
+      relogio.innerHTML = getTimeFromSeconds(segundos);
+    }, 1000);
+  }
+  document.addEventListener("click", function (event) {
+    const el = event.target;
+    if (el.classList.contains("iniciar")) {
+      relogio.classList.remove("pausado");
+      clearInterval(timer);
+      iniciaRelogio();
+    }
+    if (el.classList.contains("pausar")) {
+      relogio.classList.add("pausado");
+      clearInterval(timer);
+    }
+    if (el.classList.contains("zerar")) {
+      relogio.classList.remove("pausado");
+      clearInterval(timer);
+      relogio.innerHTML = "00:00:00";
+      segundos = 0;
+    }
+  });
 }
+relogio();
